aa45694322478e0a8a710b1bdad7a3119c93f432
Sergey Maslinikov <maslinikovsergey@gmail.com>
2021-06-03 17:12:35 -0400

    lesson5

--- hard.js
+++ hard.js
@@ -1,19 +1,33 @@
-function checkType(arg1) {
-  if (typeof arg1 !== "string") {
-    alert("Аргумент не является строкой");
+let arr = ["33", "41", "21", "32", "12", "24", "10"];
+
+for (let i = 0; i < arr.length; i++) {
+  let arrElem = arr[i];
+  let startingTwo = arrElem.indexOf("2") == 0;
+  let startingFour = arrElem.indexOf("4") == 0;
+
+  if (startingTwo || startingFour) {
+    console.log(arrElem);
+  }
+}
+
+/// 2
+let isSimpleNumber = function (n) {
+  let massOfNumbers = [];
+  for (let x = 1; x <= n; x++) {
+    let leftEl = n % x;
+    if (leftEl === 0) {
+      massOfNumbers.push(leftEl);
+    }
+  }
+  if (massOfNumbers.length === 2) {
+    return true;
   } else {
-    handelText(arg1);
+    return false;
+  }
+};
+
+for (let n = 0; n <= 100; n++) {
+  if (isSimpleNumber(n) === true) {
+    console.log(n, `Делители этого числа: 1 и ${n}`);
   }
 }
-
-function handelText(text) {
-  let newText = text.trim();
-  let limitLength = 30;
-
-  if (newText.length > limitLength) {
-    newText = newText.substring(0, limitLength) + "...";
-    console.log(newText);
-  }
-}
-
-checkType("   ss123456789012345678901234567890   ");
