fb1cbda3c5bdd2b8cb861dff27c93a3ca1ace2c6
Sergey Maslinikov <maslinikovsergey@gmail.com>
2021-06-06 12:27:09 -0400

    Lesson7

--- hard.js
+++ hard.js
@@ -1,33 +1,21 @@
-let arr = ["33", "41", "21", "32", "12", "24", "10"];
+let week = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
-for (let i = 0; i < arr.length; i++) {
-  let arrElem = arr[i];
-  let startingTwo = arrElem.indexOf("2") == 0;
-  let startingFour = arrElem.indexOf("4") == 0;
-
-  if (startingTwo || startingFour) {
-    console.log(arrElem);
+for (let i = 0; i < week.length; i++) {
+  if (week[i] !== "Saturday" || week[i] == "Sunday") {
+    console.log(week[i].italics());
+  }
+  if (week[i] == "Saturday" || week[i] == "Sunday") {
+    console.log(week[i].bold());
   }
 }
 
-/// 2
-let isSimpleNumber = function (n) {
-  let massOfNumbers = [];
-  for (let x = 1; x <= n; x++) {
-    let leftEl = n % x;
-    if (leftEl === 0) {
-      massOfNumbers.push(leftEl);
-    }
-  }
-  if (massOfNumbers.length === 2) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-for (let n = 0; n <= 100; n++) {
-  if (isSimpleNumber(n) === true) {
-    console.log(n, `Делители этого числа: 1 и ${n}`);
-  }
-}
+let day = new Date();
+console.log("Today is " + week[day.getDay()]);
